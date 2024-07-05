8c198a6f3181330273b17f7fcdb11b9b5dc4c579
giorgiodishvili <odishvili.giorgi@gmail.com>
2024-07-06 03:26:39 +0400

    improved docker-compose.yml and builds

---
+++ build.bat
@@ -0,0 +1,35 @@
+@echo off
+setlocal
+
+REM Define the repositories and their directories
+set REPO1_URL=https://github.com/giorgiodishvili/order_management
+set REPO2_URL=https://github.com/giorgiodishvili/user_management
+set REPO1_DIR=order_management
+set REPO2_DIR=user_management
+
+REM Function to clone or pull a repository
+:UPDATE_REPO
+set REPO_URL=%1
+set REPO_DIR=%2
+
+IF EXIST "..\%REPO_DIR%" (
+    echo Directory %REPO_DIR% exists. Pulling latest changes...
+    pushd "..\%REPO_DIR%"
+    git pull
+    popd
+) ELSE (
+    echo Directory %REPO_DIR% does not exist. Cloning repository...
+    pushd ..
+    git clone %REPO_URL%
+    popd
+)
+goto :EOF
+
+REM Clone or pull repositories
+call :UPDATE_REPO %REPO1_URL% %REPO1_DIR%
+echo printing %cd% pwd
+call :UPDATE_REPO %REPO2_URL% %REPO2_DIR%
+
+REM Run docker-compose up
+echo Running docker-compose up...
+docker-compose up
---
+++ build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Define the repositories and their directories
+REPO1_URL="https://github.com/giorgiodishvili/order_management"
+REPO2_URL="https://github.com/giorgiodishvili/user_management"
+REPO1_DIR="order_management"
+REPO2_DIR="user_management"
+
+# Function to clone or pull a repository
+update_repo() {
+  local repo_url=$1
+  local repo_dir=$2
+
+  if [ -d "../$repo_dir" ]; then
+    echo "Directory $repo_dir exists. Pulling latest changes..."
+    cd "../$repo_dir"
+    git pull
+
+  else
+    echo "Directory $repo_dir does not exist. Cloning repository..."
+    cd ..
+    git clone "$repo_url"
+
+  fi
+  cd ../configuration
+}
+
+# Clone or pull repositories
+update_repo "$REPO1_URL" "$REPO1_DIR"
+echo "printing $(pwd) pwd "
+update_repo "$REPO2_URL" "$REPO2_DIR"
+
+# Run docker-compose up
+echo "Running docker-compose up..."
+docker-compose up
